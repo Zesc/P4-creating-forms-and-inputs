30b1801fae0ffca9489ab54fd54620d1c3caf1f2
Zesc <jeddak.cesc@gmail.com>
2017-02-10 11:20:13 -0800

    change the look of signup form and add js file

---
+++ validator.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zesc on 2/9/17.
+ */
+
+//working on validation form for sing up form
+var bSubmit = document.getElementById("signUp");
+
+bSubmit.addEventListener('submit', function(event){
+  //event.preventDefault();
+  alert("hello");
+});
