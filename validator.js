a1aea5b7cc24c7338a5105838dbd0f737c9aa441
Zesc <jeddak.cesc@gmail.com>
2017-08-25 02:43:01 -0700

    signup form done

--- validator.js
+++ validator.js
@@ -1,11 +1,45 @@
-/**
- * Created by zesc on 2/9/17.
- */
-
 //working on validation form for sing up form
-var bSubmit = document.getElementById("signUp");
+var sForm = document.getElementById("signUp");
+var bSubmit = document.getElementById('submit');
+// var name = document.getElementById('name');
 
-bSubmit.addEventListener('submit', function(event){
-  //event.preventDefault();
-  alert("hello");
+sForm.addEventListener('submit', function(event){
+	event.preventDefault();
+	var name = document.getElementById('name');
+	var lname = document.getElementById('lname');
+	var list = [name, lname];
+
+	for (var i=0; i<list.length; i++){
+		if(list[i].value === ''){
+			list[i].className = 'my-invalid';
+			list[i].setCustomValidity("Please, fill provide information.");	  	    		
+		}else{
+		list[i].className = 'my-valid';
+		list[i].setCustomValidity('');
+		}
+	}
+}); 
+
+sForm.addEventListener('click', function(){
+	// var name = document.getElementById('name');
+	// var lname = document.getElementById('lname');
+	// var list = [name, lname];
+
+	// for (var i=0; i<list.length; i++){
+	// 	if(i.value === ''){
+	// 		name.className = 'my-invalid';
+	// 		name.setCustomValidity("Please, fill provide information.");	  	    		
+	// 	}else{
+	// 	name.className = 'my-valid';
+	// 	name.setCustomValidity('');
+	// 	}
+	// }
+	// if(name.value === ''){
+	// 	name.className = 'my-invalid';
+	// 	name.setCustomValidity("Please, fill provide information.");	  	    		
+	// }
+	// else{
+	// 	name.className = 'my-valid';
+	// 	name.setCustomValidity('');
+	// }
 });
